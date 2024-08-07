446dcd3d41ebe736513e8b502d62422dc1d52199
dransom90 <dransom90@yahoo.com>
2024-08-07 13:47:46 -0600

    test push

--- src/Components/Title.tsx
+++ src/Components/Title.tsx
@@ -4,7 +4,7 @@ function Title() {
   return (
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">Welcome to The League's Stats</h5>
+        <h5 className="card-title">Welcodfgdfgme to The League's Stats</h5>
         <p className="card-text">
           Here you will find pointless stats for our pointless (but way too
           addicting) fantasy game.
